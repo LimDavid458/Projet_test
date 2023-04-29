3ec01c8da20ad89606edc72a67ae6c02a8d27b16
Lim David <david.lim.0101@gmail.com>
2023-04-29 16:24:32 +0200

    comment script

--- demon_slayer_animation_transition/index.js
+++ demon_slayer_animation_transition/index.js
@@ -1,10 +1,11 @@
-const tl = gsap.timeline();
+const tl = gsap.timeline(); // Allows you to configure a chain animation
 const btnAnimation = document.getElementById('playAnimation');
 
 btnAnimation.addEventListener('click', () => {
     play();
 });
 
+/** Play audio and call function animation */
 function play() {
     var audio = new Audio('Katana_Saund.mp3');
     audio.volume = 1;
@@ -16,9 +17,9 @@ function play() {
 }
 
 function animation() {
-    tl.set('.layer-full', { y: '-5%', display: 'block' });
+    tl.set('.layer-full', { y: '-5%', display: 'block' }); // init position layer full
     tl.set('.layer-book, .layer-negative', { display: 'none' });
-    tl.set('#playAnimation', { pointerEvents: 'none', opacity: 0.5 });
+    tl.set('#playAnimation', { pointerEvents: 'none', opacity: 0.5 }); // button play animation disabled
 
     tl.to('.layer-full', { duration: 2, y: '-10%', display: 'none' });
     tl.to('.layer-book, .layer-negative', { duration: 0.9, display: 'block' });
