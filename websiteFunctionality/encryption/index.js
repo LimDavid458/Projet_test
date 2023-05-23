a5583dff596dcf57d51c30e4cbb98407de60ac78
Lim David <david.lim.0101@gmail.com>
2023-05-23 15:52:15 +0200

    add function to randomize letters

--- websiteFunctionality/encryption/index.js
+++ websiteFunctionality/encryption/index.js
@@ -18,7 +18,32 @@ function transformToRot13(stringToTransform) {
         }
     }
 
-    secretWord.innerText = newString;
+    randomLetter(newString);
+}
+
+function randomLetter(word) {
+    let iterations = 0;
+    secretWord.dataset.value = word.toUpperCase();
+
+    const interval = setInterval(() => {
+        secretWord.innerText = word
+            .split('')
+            .map((letter, index) => {
+                if (index < iterations) {
+                    // shift of 1 to randomize
+                    return secretWord.dataset.value[index].toLocaleUpperCase();
+                }
+                return alphabet[
+                    Math.floor(Math.random() * 26)
+                ].toLocaleUpperCase();
+            })
+            .join('');
+
+        if (iterations >= secretWord.dataset.value.length)
+            clearInterval(interval);
+
+        iterations += 1;
+    }, 100);
 }
 
 btnEncryption.addEventListener('click', (event) => {
