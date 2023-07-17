b1aa620842840d7635a16c0882751899c79cbe78
Lim David <david.lim.0101@gmail.com>
2023-07-17 17:38:49 +0200

    change animation event

--- demon_slayer_animation_transition/index.js
+++ demon_slayer_animation_transition/index.js
@@ -2,26 +2,25 @@ const tl = gsap.timeline(); // Allows you to configure a chain animation
 const btnAnimation = document.getElementById('playAnimation');
 
 btnAnimation.addEventListener('click', () => {
-    play();
+    saund();
+    animation();
 });
 
-/** Play audio and call function animation */
-function play() {
+/** Play audio */
+function saund() {
     var audio = new Audio('Katana_Saund.mp3');
     audio.volume = 1;
 
     audio.load();
     audio.play();
-
-    animation();
 }
 
 function animation() {
     tl.set('.layer-full', { y: '-5%', display: 'block' }); // init position layer full
     tl.set('.layer-book, .layer-negative', { display: 'none' });
-    tl.set('#playAnimation', { pointerEvents: 'none', opacity: 0.5 }); // button play animation disabled
+    tl.set('#playAnimation', { pointerEvents: 'none' });
 
     tl.to('.layer-full', { duration: 2, y: '-10%', display: 'none' });
     tl.to('.layer-book, .layer-negative', { duration: 0.9, display: 'block' });
-    tl.to('#playAnimation', { pointerEvents: 'auto', opacity: 1 });
+    tl.to('#playAnimation', { pointerEvents: 'auto' });
 }
